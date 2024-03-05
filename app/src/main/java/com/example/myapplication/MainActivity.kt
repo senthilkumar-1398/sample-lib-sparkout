a91fe075f6f7a2435ef7110e75a9d8f9ce6bc6d4
SenthilKumar <senthilkumar@sparkouttech.com>
2024-03-05 12:01:00 +0530

    Initial commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val apiClient:APIClientSample=APIClientSample()
-        apiClient.getClient("https://www.google.com/api/")
+//        val apiClient:APIClientSample=APIClientSample()
+//        apiClient.getClient("https://www.google.com/api/")
     }
 }
